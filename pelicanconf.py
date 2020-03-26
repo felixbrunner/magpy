5e4cc7a4b352be553847ae465a031d242995a898
felixbrunner <rubelrennfix@gmail.com>
2020-03-26 19:54:07 +0000

    clone themes, configure theme+output path

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,3 +33,9 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+
+# Additional variables
+
+THEME = 'themes/bootstrap2'
+OUTPUT_PATH = 'felixbrunner.github.io'
