a9222ff1674ddf444a00d725847d0d796a86c441
felixbrunner <rubelrennfix@gmail.com>
2020-03-27 17:50:37 +0000

    advancing settings

---
+++ README.md
@@ -0,0 +1,8 @@
+# Pelican for Python directory for personal website
+
+## Instructions
+Run the following command in the anaconda command line to assemble the website in the HTML submodule:
+
+```
+pelican -s pelicanconf.py
+```
--- pelicanconf.py
+++ pelicanconf.py
@@ -1,41 +1,82 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
+# Site metadata
 AUTHOR = 'Felix Brunner'
-SITENAME = 'magpy'
-SITEURL = ''
-
-PATH = 'content'
+SITENAME = 'Personal Website'
+SITEURL = 'https://felixbrunner.github.io'
 
 TIMEZONE = 'Europe/London'
-
 DEFAULT_LANG = 'EN'
 
-# Feed generation is usually not desired when developing
+
+# Theme settings
+THEME = 'themes/pelican-bootstrap3'
+BOOTSTRAP_THEME = 'readable'
+JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
+PLUGIN_PATHS = ['pelican-plugins']
+PLUGINS = ['i18n_subsites']
+'''
+TRIED BEFORE:
+ - blueidea: too narrow
+ - clean-blog: no page
+ - flex: clean & minimalistic, icons do not work
+ - pelican-bootstrap3: not working
+ - zurb-F5-basic: good, basicS
+'''
+STATIC_PATHS = ['img', 'static']
+FAVICON = 'img/favicon.ico'
+CUSTOM_CSS = 'static/custom.css'
+
+
+# Page content settings
+DISPLAY_PAGES_ON_MENU = True
+DISPLAY_CATEGORIES_ON_MENU = True
+TYPOGRIFY = True
+MAIN_MENU = True
+MENUITEMS = (('Home', '/home.html'),
+             ('About me', '/cv.html'),
+             )
+
+
+# Pelican output settings
+PATH = 'content'
+USE_FOLDER_AS_CATEGORY = True
+DELETE_OUTPUT_DIRECTORY = False
+OUTPUT_RETENTION = ['.git']
+OUTPUT_PATH = 'felixbrunner.github.io'
+DEFAULT_PAGINATION = 10
+
+
+# Article settings
+SHOW_ARTICLE_AUTHOR = True
+SHOW_ARTICLE_CATEGORY = True
+SHOW_DATE_MODIFIED = True
+
+
+# Feed settings
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
 
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+# External Links
+LINKS = (
+         #('Pelican', 'http://getpelican.com/'),
+         #('Python.org', 'http://python.org/'),
+         #('Jinja2', 'http://jinja.pocoo.org/'),
+         #('You can modify those links in your config file', '#'),
+         )
 
-DEFAULT_PAGINATION = 10
+# Social links
+SOCIAL = (('Github', 'https://github.com/felixbrunner'),
+          ('LinkedIn', 'https://pt.linkedin.com/in/felix-brunner-abbb786a'),
+          ('E-Mail', 'mailto: user@example.com'),
+          #('You can add links in your config file', '#'),
+          #('Another social link', '#'),
+          )
 
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+EMAIL = 'user@example.com'
 
-
-# Additional variables
-
-THEME = 'themes/flex'
-OUTPUT_PATH = 'felixbrunner.github.io'
+#icon: trophy
